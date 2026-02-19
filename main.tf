3747419b8c830f7bfb1ad1595ab33c422d161bf5
SNalla <sudheernalla.050@gmail.com>
2026-02-19 12:11:46 +0000

    fix the token issue

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = var.github_token
+}
+
+resource "github_repository" "first-repo" {
+  name        = "first-github-repo-rf"
+  description = "My first github repo using terraform"
+
+  visibility = "public"
+}
